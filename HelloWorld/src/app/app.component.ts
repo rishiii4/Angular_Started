a4a626b03ef057f97ef2f64605ab8fb66f6d4cfd
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-03-06 15:46:47 +0530

    [Pranav] Add. Display Name

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, FormsModule, NgIf], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+
 })
 export class AppComponent {
-  title = 'from BridgeLabz!';
-  imgUrl = "https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM";
+  title = 'Hello World';
+  imageUrl = "https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM";
 
   url = "http://localhost:4200/";
+  userName: string = "";
 
-  ngOnInit(): void{
-    this.title = " from BridgeLabz";
-  }
-
-  onClick($event: any){
-    console.log("Save button is clicked!", $event);
+  onClick($event: any) {
+    console.log("Button is clicked", $event);
     window.open(this.url, "_blank");
   }
 }
